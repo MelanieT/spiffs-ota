554a8688ddd26c570623508532476f5936037af5
Melanie <melanie@t-data.com>
2025-03-07 13:31:37 +0000

    Add __cplusplus guard in header

--- spiffs-ota.h
+++ spiffs-ota.h
@@ -6,6 +6,14 @@
 #ifndef LIGHTSWITCH_SPIFFS_OTA_H
 #define LIGHTSWITCH_SPIFFS_OTA_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 esp_err_t ota_spiffs(const char *serverUrl);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif //LIGHTSWITCH_SPIFFS_OTA_H
